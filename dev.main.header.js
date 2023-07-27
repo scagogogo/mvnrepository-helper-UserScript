07e9224483988a83d7fe0a49d6c6b64769e758ec
cc11001100 <cc11001100@qq.com>
2023-07-27 11:35:39 +0800

    fix: 修复隐藏文本展开时对齐的bug

---
+++ dev.main.header.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         repo1.maven.org Helper
+// @namespace    https://github.com/scagogogo/maven-repo1-helper-UserScript
+// @version      0.2
+// @description  用于辅助使用maven中央仓库
+// @document
+// @author       CC11001100
+// @match       *://repo1.maven.org/*
+// @run-at document-idle
+// @require https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// @grant       none
+// @require     file://D:/workspace/maven-repo1-helper-UserScript/main.js
+// ==/UserScript==
+(() => {
+
+    // 开发时为了方便调试使用的油猴的头 
+
+})();
