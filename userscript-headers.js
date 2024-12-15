ae4bbf64c2501304086c220850bb0a4c9e7066bf
CC11001100 <CC11001100@qq.com>
2024-12-16 01:48:23 +0800

    feat: 更细文档及版本

--- userscript-headers.js
+++ userscript-headers.js
@@ -6,8 +6,9 @@
 // @document     ${document}
 // @author       ${author}
 // @match        *://*/*
-// @require https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/jszip@2.7.0/dist/jszip.min.js
 // @run-at document-idle
 // @grant GM_xmlhttpRequest
+// @license MIT
 // ==/UserScript==
